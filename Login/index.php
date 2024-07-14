63ba8a3c71fe820b114d6c42ce0c7c06be27ae60
STEIN-99 <eric.alexis0399@gmail.com>
2024-07-14 01:51:31 +0000

    Se establece el borrador del login a travez de un form en html

---
+++ Login/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+
+    <form action="">
+
+        <h2>Inicio de sesion</h2>
+
+        <label for="">Usuario</label>
+        <input type="text">
+
+        <label for="">Contraseña</label>
+        <input type="text">
+
+        <button>Enviar</button>
+
+    </form>
+
+</div>
+
+</body>
+</html>
